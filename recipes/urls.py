8793db470ed13d84cd122ab5167dc45136cdc277
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2019-03-07 14:24:18 -0500

    sprint b creating forms

--- recipes/urls.py
+++ recipes/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('recipe/<int:recipe_id>', views.recipe_view, name='recipe'),
-    path('author/<int:author_id>', views.author_view, name='author')
+    path('author/<int:author_id>', views.author_view, name='author'),
+    path('addrecipe/', views.add_recipe),
+    path('addauthor/', views.add_author)
 ]
